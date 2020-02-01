c72c73de73fb6a27a162f100e84251fc276fd512
Sean Groff <sgroff04@gmail.com>
2020-02-01 08:06:25 -0600

    init gatsby commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Snowbank`,
+    description: `Track your journey to financial freedom with Snowbank. See your snowball progress and a view of your current debts.`,
+    author: `Sean Groff`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `snowbank`,
+        short_name: `snowbank`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
